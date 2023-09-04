d74193d565a2a5990f423d96a8a8d9acb63e5224
chaitox <ricardo.jul.88@gmail.com>
2023-09-04 00:31:54 -0400

    Initial Commit

---
+++ componentes/componentesApp.swift
@@ -0,0 +1,17 @@
+//
+//  componentesApp.swift
+//  componentes
+//
+//  Created by Ricardo on 2023-09-04.
+//
+
+import SwiftUI
+
+@main
+struct componentesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
